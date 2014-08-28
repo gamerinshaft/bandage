a5fe42f76b959afd106d98cc8c1b066be48ecfa1
j.taneichi <j.tane3ma@gmail.com>
2014-08-29 05:24:30 +0900

    時間でログアウトするようにした

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -2,7 +2,9 @@
 # The default is nothing which will include only core features (password encryption, login/logout).
 # Available submodules are: :user_activation, :http_basic_auth, :remember_me,
 # :reset_password, :session_timeout, :brute_force_protection, :activity_logging, :external
-Rails.application.config.sorcery.submodules = []
+
+# Rails.application.config.sorcery.submodules = []
+Rails.application.config.sorcery.submodules = [:session_timeout]
 
 # Here you can configure each submodule's features.
 Rails.application.config.sorcery.configure do |config|
@@ -33,11 +35,13 @@ Rails.application.config.sorcery.configure do |config|
   #
   # config.session_timeout =
 
+  config.session_timeout = 1000
+
 
   # Use the last action as the beginning of session timeout.
   # Default: `false`
   #
-  # config.session_timeout_from_last_action =
+  config.session_timeout_from_last_action = true
 
 
   # -- http_basic_auth --
