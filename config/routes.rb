8e41555f245443092244ebf6a1e23e7a990ac577
j.taneichi <j.tane3ma@gmail.com>
2014-08-29 04:26:54 +0900

    add routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -60,4 +60,10 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   resources :users
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
