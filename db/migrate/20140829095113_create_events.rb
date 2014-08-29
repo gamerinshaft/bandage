8d14e18a563a24f640bfaf08de14dd2f8c0dd2b5
j.taneichi <j.tane3ma@gmail.com>
2014-08-29 18:52:21 +0900

    add column

--- db/migrate/20140829095113_create_events.rb
+++ db/migrate/20140829095113_create_events.rb
@@ -1,10 +1,11 @@
 class CreateEvents < ActiveRecord::Migration
   def self.up
     create_table :events do |t|
+      t.integer   :user_id,   :null => false
       t.string :name
       t.datetime :start_at
       t.datetime :end_at
-      
+
       t.timestamps
     end
   end
