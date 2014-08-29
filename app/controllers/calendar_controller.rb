5ce0744fe1a542d6eac4068133056f567969bff0
j.taneichi <j.tane3ma@gmail.com>
2014-08-29 19:01:10 +0900

    change controller

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,12 +1,18 @@
 class CalendarController < ApplicationController
-  
+
   def index
     @month = (params[:month] || (Time.zone || Time).now.month).to_i
     @year = (params[:year] || (Time.zone || Time).now.year).to_i
 
     @shown_month = Date.civil(@year, @month)
 
-    @event_strips = Event.event_strips_for_month(@shown_month)
+    # @event_strips = Event.event_strips_for_month(@shown_month)
+
+    # ログインユーザーで絞り込む
+    @event_strips = Event.event_strips_for_month(
+      @shown_month,
+      0,
+      :conditions => { :user_id => current_user.id })
   end
-  
+
 end
